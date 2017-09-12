f9648e340d359aec380e62c035d14ba4d21e68ae
Paul Wooten <pawooten@gmail.com>
2017-09-12 08:52:35 -0400

    Added toolbar  component, updated flex layout. Preparing to replace with @angular/flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
 import { PatientSearchPanelComponent } from './patient-search-panel/patient-search-panel.component';
 import { PatientSearchResultsListComponent } from './patient-search-results-list/patient-search-results-list.component';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientSearchComponent,
     PatientSearchPanelComponent,
-    PatientSearchResultsListComponent
+    PatientSearchResultsListComponent,
+    AppToolbarComponent
   ],
   imports: [
     BrowserModule
