149396780639525ce15c8e0ebf1ffffca5342e5f
Paul Wooten <pawooten@gmail.com>
2017-09-14 12:33:28 -0400

    Using routerLink directive rather than click event handler

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -10,40 +10,9 @@ import { LayoutType } from '../app.enumerations';
 })
 export class ToolbarComponent implements OnInit {
 
-  private selectedLayout: LayoutType = LayoutType.ChartSearch;
-
-  // Is it necessary to declare these boolean members, or can I specify expressions in my conditional
-  // class binding?
-  private isChartSearch = false;
-  private isPatientSearch = true;
-  private isScheduledPatientSearch = false;
-
   constructor(private _router: Router ) {
-    _router.events.subscribe( eventValue => {
-      if ( eventValue instanceof NavigationEnd ) {
-        console.log('Navigation complete!');
-        console.log(eventValue.url);
-      } else {
-        console.log('Some other event!');
-      }
-    });
   }
 
   ngOnInit() {
-  }
-
-  onChartSearchClicked() {
-    console.log('chart search clicked');
-    this._router.navigate(['chartSearch']);
-  }
-
-  onPatientSearchClicked() {
-    console.log('patient search clicked');
-    this._router.navigate(['patientSearch']);
-  }
-
-  onScheduledPatientSearchClicked() {
-    console.log('scheduled patient search clicked');
-    this._router.navigate(['scheduledPatientSearch']);
   }
 }
