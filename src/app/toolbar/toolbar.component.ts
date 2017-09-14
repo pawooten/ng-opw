aeb59afa5ff84641d6c6bf63b58365a78eba1a7a
Paul Wooten <pawooten@gmail.com>
2017-09-14 08:51:20 -0400

    Toolbar selectedLayout indicator work-in-progress

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+
+import { LayoutType } from '../app.enumerations';
 
 @Component({
   selector: 'toolbar',
@@ -8,7 +10,24 @@ import { Router } from '@angular/router';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor(private _router: Router ) { }
+  private selectedLayout: LayoutType = LayoutType.ChartSearch;
+
+  // Is it necessary to declare these boolean members, or can I specify expressions in my conditional
+  // class binding?
+  private isChartSearch = false;
+  private isPatientSearch = true;
+  private isScheduledPatientSearch = false;
+
+  constructor(private _router: Router ) {
+    _router.events.subscribe( eventValue => {
+      if ( eventValue instanceof NavigationEnd ) {
+        console.log('Navigation complete!');
+        console.log(eventValue.url);
+      } else {
+        console.log('Some other event!');
+      }
+    });
+  }
 
   ngOnInit() {
   }
